33fb7b35a52ece08d8969902913a937f17e5ae7b
Joanna <joannabraccini@gmail.com>
2024-12-13 17:44:06 -0300

    organização inicial

--- src/config/services/api.service.ts
+++ src/config/services/api.service.ts
@@ -5,3 +5,4 @@ const baseURL = import.meta.env.VITE_API_BASE_URL;
 export const api = axios.create({
   baseURL: baseURL,
 });
+//https://pokeapi.co/docs/v2
---
+++ src/pages/Details.tsx
@@ -0,0 +1,17 @@
+export function Details() {
+  return (
+    <>
+      <h1>Detalhes</h1>
+    </>
+  );
+}
+/**
+ * A página de cada personagem deve ser 
+dinâmica e possuir:
+- A imagem e as informações básicas do 
+personagem: nome, id e tamanho.
+- A lista de habilidades do personagem.
+- A lista de stats (ataque, defesa, etc).
+- Um botão para voltar à tela da lista.
+
+ */
